86bb6ab5cf5d83ee1e426cffe7b37ddf8be566a6
Chace Powell <cpcpowell8@gmail.com>
2018-11-23 20:26:03 -0500

    remove comment

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const truckRoutes = require('./trucks');
 const yelpRoutes = require('./yelp');
 
-// Book routes
 router.use('/trucks', truckRoutes);
 router.use('/yelpreviews', yelpRoutes);
 
