7bcb83f62597ce34f1ebd0c9e7a667c948b18d23
Blake Robert <Blaker.blaker4@gmail.com>
2018-11-25 18:16:39 -0500

    re-add twitter webhook

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const routes = require("./routes");
 const app = express();
 const db = require("./models");
+const twitterWebhook = require('twitter-webhooks');
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
@@ -14,6 +15,52 @@ app.use(bodyParser.json());
 // }
 // Add routes, both API and view
 app.use(routes);
+
+
+//Config for Twitter Webhook	
+const webhook = twitterWebhook.userActivity({
+  serverUrl: 'https://foodtrucksniffer.herokuapp.com',
+route: '/webhook/twitter',	
+  consumerKey: process.env.TWITTER_API_KEY,	
+  consumerSecret: process.env.TWITTER_API_SECRET,	
+  accessToken: process.env.TWITTER_ACCESS_TOKEN,	
+  accessTokenSecret: process.env.TWITTER_ACCESS_SECRET,	
+  environment: process.env.TWITTER_WEBHOOK_ENV	
+});	
+
+ //Checks for registered webhook. Registers & subscribes if none is found.	
+webhook.getWebhook().then(data => {	
+  if (!data[0].valid) {	
+    webhook.register();	
+     webhook.subscribe({	
+      userId: process.env.TWITTER_USER_ID,	
+      accessToken: process.env.TWITTER_ACCESS_TOKEN,	
+      accessTokenSecret: process.env.TWITTER_ACCESS_SECRET	
+    });	
+  }	
+});	
+ //On Twitter event, update the database with new address.	
+webhook.on('event', (event, userId, data) => {	
+  const arr = data.text.split('|')[0];	
+  const address = arr	
+    .split(' ')	
+    .slice(1)	
+    .join(' ');
+    
+   db.FoodTruck.update(	
+    {	
+      address: address,	
+      addressUpdated: data.created_at	
+    },	
+    {	
+      where: {	
+        twitterId: `@${data.user.screen_name}`	
+      }	
+    }	
+  ).then(udpatedLocation => console.log(udpatedLocation));	
+});	
+ // Routes	
+app.use('/', webhook);
 
 
 const syncOptions = { force: false };
